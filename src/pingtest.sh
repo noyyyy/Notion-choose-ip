880de6d2bfc65d23c628f44f9a2b98ea0cac086d
jerrywang959 <767729940@qq.com>
2020-08-03 22:01:12 +0800

    edit

--- src/pingtest.sh
+++ src/pingtest.sh
@@ -9,7 +9,7 @@ mkdir temp
 for i in `cat out_1.txt`
 	do
 		echo 正在测试 $i
-		curl --resolve www.notion.so:443:$i https://www.notion.so/app-4ca8cd21ec209cfc38a6.js --output temp/$i -s --connect-timeout 2 --max-time 10 &
+		curl --resolve www.notion.so:443:$i https://www.notion.so/app-4ca8cd21ec209cfc38a6.js --output temp/$i -s --connect-timeout 2 --max-time 4 &
 		sleep 0.1
 	done
 sleep 5
