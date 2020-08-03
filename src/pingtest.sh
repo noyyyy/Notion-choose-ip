016ea5815627308462d2eb7f740f03500c9b86f8
jerrywang959 <767729940@qq.com>
2020-08-03 22:10:18 +0800

    update

--- src/pingtest.sh
+++ src/pingtest.sh
@@ -1,7 +1,9 @@
-fping -f avaip.txt -c 30 -i 0 > out.txt
+fping -f avaip.txt -c 50 -i 0 > out.txt
 cat out.txt  | grep "\[29\]" |  grep ", 0%"   > sort.txt 
-cat out.txt  | grep "\[29\]" |  grep ", 3%"   >> sort.txt 
+cat out.txt  | grep "\[29\]" |  grep ", 2%"   >> sort.txt 
+cat out.txt  | grep "\[29\]" |  grep ", 4%"   >> sort.txt 
 cat out.txt  | grep "\[29\]" |  grep ", 6%"   >> sort.txt 
+cat out.txt  | grep "\[29\]" |  grep ", 8%"   >> sort.txt 
 cat out.txt  | grep "\[29\]" |  grep ", 10%"   >> sort.txt 
 cut -f1 -d: sort.txt  > out_1.txt
 
@@ -10,7 +12,7 @@ for i in `cat out_1.txt`
 	do
 		echo 正在测试 $i
 		curl --resolve www.notion.so:443:$i https://www.notion.so/app-4ca8cd21ec209cfc38a6.js --output temp/$i -s --connect-timeout 2 --max-time 4 &
-		sleep 0.1
+		sleep 0.01
 	done
 sleep 5
 
