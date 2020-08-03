9b51abdb72bacd67fe82d4e0254426c7e409157d
jerrywang959 <767729940@qq.com>
2020-08-03 22:05:36 +0800

    update

--- src/pingtest.sh
+++ src/pingtest.sh
@@ -14,16 +14,10 @@ for i in `cat out_1.txt`
 	done
 sleep 5
 
-ls -Slrh --block-size=k temp > speed.txt
-sort speed.txt -o sort_3.txt -d -k 5rn
-sed '2,$d' sort_3.txt > finalip.txt
-cut -f11 -d " "  finalip.txt > ip.txt
+ls -Slrh --block-size=k temp | sort -d -k 5rn | sed '2,$d' | cut -f11 -d " " > ip.txt
 cat ip.txt
 
 rm -rf temp
 rm out.txt
-rm  sort_3.txt
 rm sort.txt
 rm out_1.txt
-rm speed.txt
-rm finalip.txt
